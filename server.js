b379a766a55f1efe70ed0bcde21151946e192b76
KhoiTran <zzkhoizz123@gmail.com>
2018-11-15 22:21:09 +0700

    init

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+//const path = require('path');
+
+const users = require('./routes/api/users');
+
+const app = express();
+
+// Bodyparser Middleware
+app.use(bodyParser.json());
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to Mongo
+mongoose
+  .connect(db, {useNewUrlParser: true}) // Adding new mongo url parser
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+// Use Routes
+app.use('/api/users', users);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, ()=> console.log(`Server started on port ${port}`));
