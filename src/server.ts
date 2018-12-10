33c6f5c47b237aef1027edf9775c9cdb45f4ca46
Nguyễn Anh Khoa <ng.akhoa@yahoo.com.vn>
2018-12-10 17:04:25 +0700

    add logger and test

--- src/server.ts
+++ src/server.ts
@@ -3,31 +3,32 @@ import express = require('express');
 import mongoose = require('mongoose');
 import bodyParser = require('body-parser');
 import cors = require('cors');
+import {factory} from './config/LoggerConfig';
 
 import {router as user_route} from './api/v1/users';
 import {development as db_dev, production as db_prod} from './config/keys';
 
 const app = express();
 const port = process.env.PORT || 5000;
+const dbLog = factory.getLogger("database.Mongo");
+const routeLog = factory.getLogger("request.Route");
+const db = process.env.PORT ? db_prod : db_dev;
 
 app.use(cors());
 app.use(bodyParser.json());
 
 // Connect to Mongo
-let db = '';
-if (process.env.PORT) {
-    db = db_prod;
-} else {
-    db = db_dev;
-}
 mongoose
     .connect(db, {useNewUrlParser: true})  // Adding new mongo url parser
-    .then(() => console.log('MongoDB Connected...'))
+    .then(() => dbLog.info('MongoDB Connected...'))
     .catch(err => {
         // console.log(err);
-        console.log("Cannot connect to db: ${db}");
+        // console.log("Cannot connect to db: ${db}");
+        dbLog.error(err);
+        dbLog.error("Cannot connect to db: " + db);
+        process.exit(1);
     });
 
 app.use('/api/users', user_route);
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => routeLog.info(`Server started on port ${port}`));
