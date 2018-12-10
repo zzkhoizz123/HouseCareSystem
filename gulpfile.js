7ddb45667ffb7937c61d919dd41c87897619ec4a
Nguyễn Anh Khoa <ng.akhoa@yahoo.com.vn>
2018-12-10 17:04:25 +0700

    add logger and test

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,20 @@
-var gulp = require('gulp');
-var nodemon = require('gulp-nodemon');
-var ts = require('gulp-typescript');
-var tsProject = ts.createProject('tsconfig.json');
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+const notify = require('gulp-notify');
+const nodemon = require('gulp-nodemon');
+const mocha = require('gulp-mocha');
+const tsProject = ts.createProject('tsconfig.json');
 
 gulp.task('build', function() {
     return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest('built'));
 });
 
-gulp.task('start', (cb) => {
-    nodemon({
-        script: 'built/server.js',
-        watch: ["built/**/*.js"],
-        ext: "js"
-    }).on('restart', () => {
-        gulp.src('server.js')
-            .pipe(notify('AAAAAA'));
-    });
-})
+gulp.task('start', () => {
+    nodemon({script: 'built/server.js', watch: ['built/**/*.js'], ext: 'js'})
+        .on('restart', () => { gulp.src('server.js').pipe(notify('AAAAAA')); });
+});
+
+gulp.task('test', () => {
+    return gulp.src('test/**/*.spec.ts')
+        .pipe(mocha({reporter: 'nyan', require: ['ts-node/register']}));
+});
