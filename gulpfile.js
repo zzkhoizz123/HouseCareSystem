5cad5669881f98eb79e4e790fd06737c39b1c165
Nguyễn Anh Khoa <ng.akhoa@yahoo.com.vn>
2018-12-13 01:09:49 +0700

    gulp start fix (run directly by ts-node with tsconfig-paths)

--- gulpfile.js
+++ gulpfile.js
@@ -10,21 +10,42 @@ const abspath = require('gulp-absolute-path');
 // const notify = require('gulp-notify');
 
 const tsProject = ts.createProject('tsconfig.json');
+require('ts-node').register(tsProject);
 
 process.env.TS_NODE_PROJECT = './tsconfig.json';
 process.env.TS_CONFIG_PATHS = true;
 
-gulp.task('build', function() {
-    return tsProject.src()
-        .pipe(abspath({rootDir: './src'}))
-        .pipe(tsProject())
-        .pipe(gulp.dest('built'));
-});
+// gulp.task('build', function() {
+//     return tsProject.src()
+//         .pipe(abspath({rootDir: './src'}))
+//         .pipe(tsProject()).js
+//         .pipe(gulp.dest('built'));
+// });
 
 gulp.task('start', () => {
-    nodemon(
-        {script: 'built/src/server.js', watch: ['built/**/*.js'], ext: 'js'})
-        .on('restart', () => { gulp.src('built/server.js'); });
+    return nodemon(
+            {
+                script: 'src/server.ts',
+                watch: ['src/**/*.ts'],
+                ext: 'ts',
+                exec: 'ts-node -r tsconfig-paths/register',
+            });
+        // .on('restart', () => { gulp.src('built/server.js'); });
+});
+
+gulp.task('prod', () => {
+    return nodemon(
+            {
+                script: 'src/server.ts',
+                watch: ['src/**/*.ts'],
+                ext: 'ts',
+                exec: 'ts-node -r tsconfig-paths/register',
+                env: {
+                    // setup for production build
+                    "PORT": 8080
+                }
+            });
+        // .on('restart', () => { gulp.src('built/server.js'); });
 });
 
 gulp.task('test', () => {
