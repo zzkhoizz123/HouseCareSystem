a4307c65b6d31a14c2934a2357d9609cc44f5224
Nguyễn Anh Khoa <ng.akhoa@yahoo.com.vn>
2018-12-13 22:30:53 +0700

    Refind WorkModel code
    Merge branch 'HEAD' into origin/work-api

--- src/models/WorkModel.ts
+++ src/models/WorkModel.ts
@@ -1,12 +1,8 @@
-import * as bcrypt from "bcryptjs";
 import * as Promise from "bluebird";
 import { ObjectId } from "bson";
-import { model, Schema } from "mongoose";
 
 import { factory } from "config/LoggerConfig";
 import * as Model from "models/Models";
-import { Work } from "./Work";
-import { help } from "typescript-logging";
 
 const dbLog = factory.getLogger("database.Mongo");
 const routeLog = factory.getLogger("request.Route");
@@ -14,24 +10,21 @@ const UserModel = Model.UserModel;
 const WorkModel = Model.WorkModel;
 
 const CreateWork = (
-  userId,
-  typeList,
-  newtime,
-  newsalary,
-  newlocation,
-  newdescription
-) => {
+    userId,
+    typeList,
+    newtime,
+    newsalary,
+    newlocation,
+    newdescription
+  ) => {
   return new Promise((resolve, reject) => {
     UserModel.findOne(
       {
         $and: [{ _id: userId }, { role: 1 }]
-      },
-      (err, user) => {
-        if (err) {
-          return reject("Error occur");
-        }
+      })
+      .then((user) => {
         if (!user) {
-          return reject("No user found");
+          return reject("User not found");
         }
         const work = new WorkModel({
           type: typeList,
@@ -43,113 +36,90 @@ const CreateWork = (
           owner: user._id,
           helper: null
         });
-        WorkModel.create(work).then(newwork => {
-          UserModel.update(
-            { _id: user._id },
-            { $push: { workingList: newwork._id } }
-          );
-          WorkModel.findById(newwork._id)
-            .populate({
-              path: "owner",
-              select: "-password -__v -role",
-              model: "User"
-            })
-            .exec((err1, res) => {
-              if (err1) {
-                return reject(err1);
-              }
-              return resolve(res);
-            });
-        });
-      }
-    );
-  });
+        WorkModel.create(work)
+          .then(newwork => {
+            UserModel.update(
+              { _id: user._id },
+              { $push: { workingList: newwork._id } }
+            );
+            WorkModel.findById(newwork._id)
+              .populate({
+                path: "owner",
+                select: "-password -__v -role",
+                model: "User"
+              })
+              .exec((err1, res) => {
+                if (err1) {
+                  return reject(err1);
+                }
+                return resolve(res);
+              });
+          });
+      })
+      .catch(err => {
+        return reject("Error occured");
+      })
+    });
 };
 
-const ChooseWork = (userId, workId) => {
+const ChooseWork = (userId, userRole, workId) => {
   return new Promise((resolve, reject) => {
-    UserModel.findById(userId, (err, user) => {
+    if (userRole !== 0) {
+      return reject("You are not a worker");
+    }
+    WorkModel.findOneAndUpdate(
+      { _id: new ObjectId(workId) },
+      { $set: { helper: userId } }
+    )
+    .populate({
+      path: "owner",
+      select: "-password -__v -role",
+      model: "User"
+    })
+    .populate({
+      path: "helper",
+      select: "-password -__v -role",
+      model: "User"
+    })
+    .exec((err, work) => {
       if (err) {
         return reject("Error occur");
       }
-      if (!user) {
-        return reject("No user found");
-      }
-      if (user.role !== 0) {
-        return reject("You are not a worker");
-      }
-      WorkModel.updateOne(
-        { _id: new ObjectId(workId) },
-        { $set: { helper: user._id } },
-        (err2, raw) => {
-          if (err2) {
-            reject("Error in update database");
-          }
-          WorkModel.findById(workId)
-            .populate({
-              path: "owner",
-              select: "-password -__v -role",
-              model: "User"
-            })
-            .populate({
-              path: "helper",
-              select: "-password -__v -role",
-              model: "User"
-            })
-            .exec((err1, work) => {
-              if (err1) {
-                return reject("Error occur");
-              }
-              return resolve(work);
-            });
-        }
-      );
+        return resolve(work);
     });
   });
 };
 
-const GetWorkingListOfUser = userId => {
+const GetWorkList = (userId, userRole, query) => {
   return new Promise((resolve, reject) => {
-    UserModel.findById(userId, (err, user) => {
+    let userQuery: object;
+    if (userRole === 0) {
+      userQuery = {helper: new ObjectId(userId)};
+    }
+    else {
+      userQuery = {owner: new ObjectId(userId)};
+    }
+    WorkModel.find({
+      $and: [
+        userQuery,
+        query
+      ]
+    }, (err, lst) => {
       if (err) {
-        return reject("Error occur");
+        return reject("Error when updating database");
       }
-      if (!user) {
-        return reject("No user found");
-      }
-      if (user.role === 0) {
-        WorkModel.find(
-          {
-            $and: [
-              { helper: new ObjectId(userId) },
-              { time: { $gt: Date.now() } }
-            ]
-          },
-          (err1, lst) => {
-            if (err1) {
-              return reject(err1);
-            }
-            return resolve(lst);
-          }
-        );
-      } else {
-        WorkModel.find(
-          {
-            $and: [
-              { owner: new ObjectId(userId) },
-              { time: { $gt: Date.now() } }
-            ]
-          },
-          (err1, lst) => {
-            if (err1) {
-              return reject("Error in find");
-            }
-            return resolve(lst);
-          }
-        );
-      }
+      return resolve(lst);
     });
   });
 };
 
-export { CreateWork, GetWorkingListOfUser, ChooseWork };
+const GetWorkingListOfUser = (userId, userRole) => {
+  return GetWorkList(userId, userRole, {time: {$gt: Date.now()}});
+};
+
+export {
+  CreateWork,
+  GetWorkingListOfUser,
+  ChooseWork,
+  GetWorkList
+};
